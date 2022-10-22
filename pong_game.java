6ed9291775172ef5d472f9ca16ce7fb3e23e3fdb
KatyayaniKommoju <111505050+KatyayaniKommoju@users.noreply.github.com>
2022-10-22 19:33:05 +0530

    Add files via upload

---
+++ pong_game.java
@@ -0,0 +1,15 @@
+package pong_game;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+public class pong_game  
+{
+	public static void main(String[] args)
+	{
+		GameFrame frame=new GameFrame();
+		
+	}
+}
+	
